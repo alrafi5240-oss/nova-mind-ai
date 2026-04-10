d9c4f6b354dfdab77cb5855684771a6e60cd289f
MD SHAKIL MIA <shakil@shakils-MacBook-Air.local>
2026-04-10 06:24:30 +0600

    fix vite blank page

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
-import App from './App';
+import App from './App.jsx';
 import { registerServiceWorker } from './utils/registerServiceWorker';
 
 registerServiceWorker();
